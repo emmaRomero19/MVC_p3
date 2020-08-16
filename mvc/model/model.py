5aa46bf9b65d3aea1228f69781426e55357e126f
Emma Romero <emmayrs17@gmail.com>
2020-08-15 20:28:56 -0500

    conexion a base de datos

---
+++ mvc/model/model.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+class Alumnos():
+    def connect(self):
+        try:
+            self.cnx = mysql.connector.connect(
+                user='user_agenda', 
+                password='Agenda_2020',
+                host='127.0.0.1',
+                port=3306,
+                database='escuela')
+            print("Conectado")
+            self.cursor = self.cnx.cursor()
+        except Exception as e:
+            print("Error connect: ", e)
+    
+    
+objeto=Alumnos()
+objeto.connect()
