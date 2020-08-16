9a4e585297107f5cbf6ecea61a8faba2da300456
Emma Romero <emmayrs17@gmail.com>
2020-08-15 20:30:21 -0500

    metodo select

--- mvc/model/model.py
+++ mvc/model/model.py
@@ -15,6 +15,34 @@ class Alumnos():
         except Exception as e:
             print("Error connect: ", e)
     
-    
+    def select(self):
+        try:
+            self.connect()
+            query=('SELECT * FROM alumnos;')
+            self.cursor.execute(query)
+            result=[]
+            for row in self.cursor:
+                r = {
+                    'id_persona' : row[0],
+                    'nombre' : row[1],
+                    'ap_paterno' : row[2],
+                    'ap_materno' : row[3],
+                    'edad' : row[4],
+                    'fecha_nac' : row[5],
+                    'genero' : row[6],
+                    'estado' : row[7],
+                    }
+                result.append(r)
+            self.cursor.close()
+            self.cnx.close()
+            return result
+        except Exception as e:
+            print("Error Select: ", e)
+            result =[]
+            return result
+
 objeto=Alumnos()
 objeto.connect()
+
+for row in objeto.select():
+    print(row)
